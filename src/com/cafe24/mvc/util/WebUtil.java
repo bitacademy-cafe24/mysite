6c5e56facb41a986e1eeef4ac54c41409731611d
D. Hyuck Ahn <kickscar@gmail.com>
2018-03-15 15:43:04 +0900

    init commit

---
+++ src/com/cafe24/mvc/util/WebUtil.java
@@ -0,0 +1,39 @@
+package com.cafe24.mvc.util;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WebUtil {
+
+	public static Integer checkParameter( String value, Integer defaultValue ) {
+		return defaultValue;
+	}
+	
+	public static Long checkParameter( String value, Long defaultValue ) {
+		return defaultValue;
+	}
+	
+	public static String checkParameter( String value, String defaultValue ) {
+		return defaultValue;
+	}
+	
+	public static void redirect(
+		HttpServletRequest request,
+		HttpServletResponse response,
+		String url ) throws ServletException, IOException {
+		response.sendRedirect( url );
+	}
+	
+	public static void forward(
+		HttpServletRequest request,
+		HttpServletResponse response,
+		String path) throws ServletException, IOException {
+		RequestDispatcher rd = 
+				request.getRequestDispatcher( path );
+			rd.forward(request, response);
+	}
+}
